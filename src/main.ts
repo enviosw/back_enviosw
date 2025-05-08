1b71dca43b1b212ead7748c47e783f3fb84b15fa
millerbermeo <millerbermeo30@gmail.com>
2025-05-08 00:16:30 -0500

    commit api

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api'); // 👈 esto es lo que necesitas
+  //Comentario
 
   app.enableCors({
     origin: '*', // Permite cualquier origen
