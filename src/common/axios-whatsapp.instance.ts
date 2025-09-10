efeaa3ed5a11aba42d891e0df50f260538fe6003
millerbermeo <millerbermeo30@gmail.com>
2025-09-10 13:19:21 -0500

    commit ajuets 3

--- src/common/axios-whatsapp.instance.ts
+++ src/common/axios-whatsapp.instance.ts
@@ -1,6 +1,6 @@
 // src/shared/http/axios-whatsapp.instance.ts
 import axios from 'axios';
-import { whatsappConstants } from 'src/auth/constants/jwt.constant'; // Ajusta el path si es diferente
+import { whatsappConstants } from '../auth/constants/jwt.constant'; // Ajusta el path si es diferente
 
 export const axiosWhatsapp = axios.create({
   baseURL: `${whatsappConstants.baseUrl}/${whatsappConstants.apiVersion}/${whatsappConstants.phoneId}`,
