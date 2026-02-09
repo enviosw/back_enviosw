43412e8279ea09ad0da5b8479a56b450e95c3365
millerbermeo <millerbermeo30@gmail.com>
2026-02-08 21:28:56 -0500

    commit ecosytem v2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,8 @@ module.exports = {
     {
       name: "enviosw-api",
       script: "dist/src/main.js",
-      instances: 1,
+      exec_mode: "fork",     // ✅ fuerza fork
+      instances: 1,          // ✅ se puede dejar o quitar
       autorestart: true,
       watch: false,
       max_memory_restart: "300M",
